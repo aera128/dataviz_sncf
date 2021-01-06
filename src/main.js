a3f585b190fc5e5482468fc120a78249930c3d8b
aera128 <matthmercier8@gmail.com>
2021-01-06 07:13:41 +0100

    Project Setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
